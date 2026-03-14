709217f5d47d6e74bc3956b68f27953fd9e7d992
Tarsmio <tarsmio@gmail.com>
2026-03-14 17:56:21 +0100

    Implementin socket server and db

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
+// server.js
 require('dotenv').config()
 
 const http = require('http')
 const app = require('./app')
+const socket = require('./socket')
+
+const mongoose = require('mongoose')
 
 const {normalizePort} = require('./utils/serverUtils')
 const logger = require('./logger')
@@ -14,37 +18,38 @@ const errorHandler = error => {
     if (error.syscall !== 'listen') {
         throw error;
     }
-
     const address = server.address();
-
     const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
 
     switch (error.code) {
-
         case 'EACCES':
-
             logger.error(bind + ' requires elevated privileges.')
             process.exit(1);
             break;
-
         case 'EADDRINUSE':
             logger.error(bind + ' is already in use.')
             process.exit(1);
             break;
-
         default:
             throw error;
     }
 }
 
+mongoose.connect(process.env.DB_LINK, { dbName: process.env.DB_NAME}).then(() => {
+    console.log("Connexion DB reussi !")
+}).catch((err) => {
+    console.log(err)
+    console.log("Connexion Db raté !")
+})
 
 const server = http.createServer(app)
+socket.init(server)
 
 server.on('error', errorHandler)
 server.on('listening', async () => {
     await cacheTeams()
 
-    const adress = server.address()
+    const address = server.address()
     const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
     logger.info('Listening on ' + bind)
 })
